eefb84fe5f9b1a5dd3f10f24d3dd3c778c270914
inho2736 <inho2736@gmail.com>
2021-04-28 14:24:00 +0900

    code : Programmers 문자열 압축

---
+++ Inho/Programmers/Week8/Programmers2_문자열압축.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class dsfdsf{
+    public static void main(String[] args){
+        
+    }
+    public static int solution(String s) {
+        int answer = s.length();
+        int length = s.length();
+        for(int i=1; i<= length/2; i++){
+            String bfStr = "";
+            int reCnt = 1;
+            int curLength = length;
+            int numLength = 0;
+            
+            for(int j=0; j+i <=length; j += i){
+                String cur = s.substring(j, j+i);
+                if(bfStr.equals(cur)){
+                    if(reCnt == 1){
+                        curLength -= (i - 1);
+                    }else{
+                        if((int)Math.log10(reCnt+1) > numLength){
+                            curLength += 1;
+                            numLength += 1;
+                        }
+                        curLength -= i;
+                    }
+                    reCnt ++;
+                }else{
+                    bfStr = cur;
+                    reCnt = 1;
+                    numLength = 0;
+                }
+            }
+            answer = Math.min(answer, curLength);
+            
+        }
+        return answer;
+    }
+    
+}
